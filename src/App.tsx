d9ede21cad5c8c579390cdbff1b6183090f95cd0
Patrice <pmaxwell30@gatech.edu>
2025-12-21 22:06:11 -0500

    Working useFetch: takes url, returns data loading, error state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import { useFetch } from "./utils/useFetch";
+
 
 const App = () => {
+
+// const { data, loading, error } = useFetch('www.themealdb.com/api/json/v1/1/random.php');
+const { data, loading, error } = useFetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Errorhere: {error}</p>;
+  console.log('nt')
+  console.log(data)
   return (
     <div>
       App
-      
+
     </div>
   );
 }
---
+++ src/utils/useFetch.ts
@@ -0,0 +1,46 @@
+import{ useEffect, useState } from 'react';  
+  
+export function useFetch(url: string){
+    const [data , setData] = useState(null)
+    const [loading , setLoading] = useState(true)
+    const [error , setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        let isMounted = true
+
+        async function fetchData() {
+        try{
+            setLoading(true)
+            const response = await fetch(url)
+
+            
+            if(!response.ok){
+                throw new Error("Dis Broke")
+            }
+            
+            console.log(response )
+            const result = await response.json()
+            console.log(result )
+
+            if (isMounted){
+            setData(result)
+            setError(null)
+            }
+        } catch (err){
+            if (isMounted && err instanceof Error){
+            setError(err.message)
+            }
+        } finally{
+            if (isMounted){
+            setLoading(false)
+            }
+        }       
+        }
+
+        fetchData()
+
+        return () =>{ isMounted = false}
+    },[url])
+
+    return { data , loading ,error}
+}
